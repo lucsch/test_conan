e866c2fe5dccfad4fc6a0d51f3dfaba4c5c655f8
Lucien Schreiber <lucien.schreiber@terranum.ch>
2022-04-03 10:40:18 +0200

    Improving test_geos for showing the bug in GDAL

--- test_geos/src/main.cpp
+++ test_geos/src/main.cpp
@@ -1,20 +1,22 @@
-#include <iostream>
-#ifndef HAVE_GEOS
-#define HAVE_GEOS
-#endif
 #include "gdal_priv.h"
 #include "ogr_geometry.h"
+#include <iostream>
 
-int main(int argc, const char* argv[]){
+int main(int argc, const char *argv[]) {
   std::cout << "Hello world from conan" << std::endl;
   GDALAllRegister();
+  if (OGRGeometryFactory::haveGEOS()) {
+    std::cout << "GEOS OK!" << std::endl;
+  } else {
+    std::cout << "NO GEOS IN GDAL!" << std::endl;
+  }
 
-//  OGRGeometry * p1 = nullptr;
-//  OGRGeometry * p2 = nullptr;
-//  OGRGeometry * p3 = nullptr;
-//  OGRGeometryFactory::createFromWkt("POINT(10 20)", NULL, &p1);
-//  OGRGeometryFactory::createFromWkt("POINT(30 20)", NULL, &p2);
-//  p3 = p1->Union(p2);
-  std::cout << "GEOS" << OGRGeometryFactory::haveGEOS() << std::endl ;
+  // testing union (GEOS is needed)
+  OGRGeometry *p1 = nullptr;
+  OGRGeometry *p2 = nullptr;
+  OGRGeometry *p3 = nullptr;
+  OGRGeometryFactory::createFromWkt("POINT(10 20)", NULL, &p1);
+  OGRGeometryFactory::createFromWkt("POINT(30 20)", NULL, &p2);
+  p3 = p1->Union(p2);
   return 0;
 }
