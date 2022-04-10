02bfc49d7cfc44f30dd49629b99bed6a3de95b94
Lucien Schreiber <lucien.schreiber@terranum.ch>
2022-04-10 21:14:24 +0200

    bump version to wxWidgets 3.1.6

--- basic_wxwidgets/conanfile.py
+++ basic_wxwidgets/conanfile.py
@@ -3,11 +3,11 @@ from conans import ConanFile, CMake
 
 class BasicwxWidgets(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    requires = ["wxwidgets/3.1.5@bincrafters/stable"]
+    requires = ["wxwidgets/3.1.6@terranum-conan+wxwidgets/stable"]
     generators = "cmake", "gcc", "txt"
 
     # default_options = {"wxwidgets:shared": True}
-    default_options = {"wxwidgets:webview": False}
+    #default_options = {"wxwidgets:webview": False}
 
     def imports(self):
         self.copy("*.dll", dst="bin", src="bin")  # From bin to bin
